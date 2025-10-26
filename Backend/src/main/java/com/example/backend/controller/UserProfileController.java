b281831ad114a0bf1c05ec035bafa70ab7fcc0a7
Khac-Quan <quanvuongy1986@gmail.com>
2025-10-26 15:36:12 +0700

    change-password userprofile and update profile

--- Backend/src/main/java/com/example/backend/controller/UserProfileController.java
+++ Backend/src/main/java/com/example/backend/controller/UserProfileController.java
@@ -1,10 +1,16 @@
 package com.example.backend.controller;
 
+import com.example.backend.dto.ApiResponse;
+import com.example.backend.dto.ChangePasswordRequest;
 import com.example.backend.dto.UserProfileResponse;
+import com.example.backend.dto.UserProfileUpdateRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public interface UserProfileController {
 
     public ResponseEntity<Object> showProfile(@PathVariable String id);
+    public ResponseEntity<ApiResponse> changePassword(@PathVariable String username,@RequestBody ChangePasswordRequest request);
+    public ResponseEntity<ApiResponse> updateProfile(@PathVariable String username,@RequestBody UserProfileUpdateRequest request);
 }
